bd571e53dec12abac62222fb3ac91357c65917bb
Brian-Mas <brianmasschaele@icloud.com>
2016-09-21 21:11:45 +0200

    updated mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/brianmasschaele@icloud.com",
                 type: "POST",
                 data: {
                     name: name,
